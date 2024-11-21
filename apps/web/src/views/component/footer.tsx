217057df62742dc2577b0cbfc213f705db27b8b3
SHarishP <setiawanharish85@gmail.com>
2024-11-21 08:04:20 +0700

    fix: repair package.json in root

--- apps/web/src/views/component/footer.tsx
+++ apps/web/src/views/component/footer.tsx
@@ -1,41 +1,20 @@
-import React from 'react';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
-const Footer: React.FC = () => {
+export default function Footer() {
   return (
     <footer className="bg-[#21003A] text-gray-300 py-12 w-full">
       <div className="px-8 lg:px-16">
         <div className="flex flex-wrap justify-between">
+          {/* Web profile short */}
           <div className="w-full md:w-1/3 mb-8 md:mb-0">
             <h2 className="text-3xl font-bold text-white mb-4">Eventsu.</h2>
             <p className="mb-4">
               Purchase with us and we will ensure a seamless experience for you.
               What are you waiting for? Book your events on Eventsu today!
             </p>
-            {/* <div className="flex space-x-4">
-              <img src="/path/to/visa.svg" alt="Visa" className="h-6" />
-              <img
-                src="/path/to/mastercard.svg"
-                alt="MasterCard"
-                className="h-6"
-              />
-              <img src="/path/to/gpay.svg" alt="Google Pay" className="h-6" />
-              <img
-                src="/path/to/applepay.svg"
-                alt="Apple Pay"
-                className="h-6"
-              />
-              <img src="/path/to/grabpay.svg" alt="Grab Pay" className="h-6" />
-              <img src="/path/to/paynow.svg" alt="PayNow" className="h-6" />
-            </div> */}
-            {/* <p className="mt-4">
-              See our <span className="font-semibold">140 reviews</span> on{' '}
-              <a href="#" className="text-green-500 font-semibold">
-                Trustpilot
-              </a>
-            </p> */}
           </div>
 
+          {/* Company Column
           <div className="w-full md:w-1/4 mb-8 md:mb-0">
             <h3 className="text-white font-semibold mb-4">COMPANY</h3>
             <ul>
@@ -55,24 +34,11 @@ const Footer: React.FC = () => {
                 </a>
               </li>
             </ul>
-          </div>
-          {/* <div className="w-full md:w-1/4 mb-8 md:mb-0">
-            <h3 className="text-white font-semibold mb-4">HELP & SUPPORT</h3>
-            <ul>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Contact Us
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Support
-                </a>
-              </li>
-            </ul>
           </div> */}
+
+          {/*Legal Column  */}
           <div className="w-full md:w-1/4">
-            <h3 className="text-white font-semibold mb-4">LEGAL</h3>
+            <h3 className="text-white font-semibold mb-4">MORE FROM US</h3>
             <ul>
               <li>
                 <a href="#" className="hover:text-white">
@@ -87,6 +53,11 @@ const Footer: React.FC = () => {
               <li>
                 <a href="#" className="hover:text-white">
                   Terms and Conditions
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">
+                  Create Event Here!
                 </a>
               </li>
             </ul>
@@ -109,6 +80,4 @@ const Footer: React.FC = () => {
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
--- apps/web/src/views/component/navbar.tsx
+++ apps/web/src/views/component/navbar.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useState } from 'react';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
+import { useState } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,34 +12,36 @@ export default function Navbar() {
   };
 
   return (
+    <div className="flex flex-row justify-between bg-white h-14 items-center mt-6">
+      <div className="flex flex-row items-center">
+        <Link href="/">
+          <img src="logo" alt="ini Logo" />
+        </Link>
+      </div>
 
- <div className="flex flex-row justify-between bg-white h-14 items-center mt-6">
-  <div className='flex flex-row items-center'>
-    <Link href="/">
-    <img src="logo" alt="ini Logo" />
-    </Link>
-  </div>
+      <div>
+        <button className="hover:bg-blue-500 mx-4">Find Events</button>
+        <button className="hover:bg-blue-500 mx-4">Create Event</button>
+        <button className="hover:bg-blue-500 mx-4">Find my Tickets</button>
 
-<div>
- <button className="hover:bg-blue-500 mx-4">Find Events</button>
- <button className="hover:bg-blue-500 mx-4">Create Event</button>
- <button className="hover:bg-blue-500 mx-4">Find my Tickets</button>
-  
-  <input type="text" name="" id="" className="rounded-md bg-gray-300 mx-4" placeholder='Seacrh Event...'/>
-  <button className="bg-blue-700 p-1 px-5 text-black rounded-full hover:bg-blue-600">
-    <FontAwesomeIcon icon={faSearch} />
-  </button>
-  
-</div>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="rounded-md bg-gray-300 mx-4"
+          placeholder="Seacrh Event..."
+        />
+        <button className="bg-blue-700 p-1 px-5 text-black rounded-full hover:bg-blue-600">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
 
-    <div>
-    <button className="hover:bg-blue-500 mx-4">Login</button>
-    <button className="bg-blue-800 p-2 px-5 text-white rounded-full hover:bg-blue-300 mx-4">Sign Up</button>
-    </div> 
-    
-
- </div>
- 
- 
-  )
+      <div>
+        <button className="hover:bg-blue-500 mx-4">Login</button>
+        <button className="bg-blue-800 p-2 px-5 text-white rounded-full hover:bg-blue-300 mx-4">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 }
