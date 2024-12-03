9995ab6cae3f0c0819ff4660b4cdccd02f2d3fdb
SHarishP <setiawanharish85@gmail.com>
2024-12-03 17:16:35 +0700

    feat: add events dashboard for eo

--- apps/api/src/routes/transaction.routes.ts
+++ apps/api/src/routes/transaction.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { VerifyToken } from "../middlewares/user.middleware";
+import { VerifyToken, CustGuard } from "../middlewares/user.middleware";
 import {
   Checkout,
   DeleteTransaction,
@@ -12,18 +12,18 @@ import {
 
 const router = Router();
 // Book Ticket
-router.post("/checkout", VerifyToken, Checkout);
+router.post("/checkout", VerifyToken, CustGuard, Checkout);
 // Delete Booked Ticket
-router.delete("/:transactionId", VerifyToken, DeleteTransaction);
+router.delete("/:transactionId", VerifyToken, CustGuard, DeleteTransaction);
 // Getting unpaid transaction by userId
-router.get("/cart", VerifyToken, GetTransactionsByUser);
+router.get("/cart", VerifyToken, CustGuard, GetTransactionsByUser);
 // Get transaction done by user
-router.get("/done", VerifyToken, GetPaidTransactionsByUser);
+router.get("/done", VerifyToken, CustGuard, GetPaidTransactionsByUser);
 // Process Transaction(s)
-router.post("/payment", VerifyToken, MakePayment);
+router.post("/payment", VerifyToken, CustGuard, MakePayment);
 // Get Payment
-router.get("/payment", VerifyToken, GetPaymentsByUser);
+router.get("/payment", VerifyToken, CustGuard, GetPaymentsByUser);
 // Upload payment proof
-router.post("/update", VerifyToken, UploadPaymentProof);
+router.post("/update", VerifyToken, CustGuard, UploadPaymentProof);
 
 export default router;
